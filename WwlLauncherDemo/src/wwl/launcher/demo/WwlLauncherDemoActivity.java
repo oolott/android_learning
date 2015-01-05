b52d0458741c25a3ae029f6afda6bde6a7cf2afd
358583488@qq.com <358583488@qq.com>
2015-01-05 23:58:05 +0800

    动态添加桌面快捷方式

--- WwlLauncherDemo/src/wwl/launcher/demo/WwlLauncherDemoActivity.java
+++ WwlLauncherDemo/src/wwl/launcher/demo/WwlLauncherDemoActivity.java
@@ -1,12 +1,26 @@
 package wwl.launcher.demo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WwlLauncherDemoActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
+		findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent _returnIntent = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
+				_returnIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, "wwlAuto");
+//				_returnIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, Intent.ShortcutIconResource.fromContext(WwlLauncherDemoActivity.this, R.drawable.sms_voice_input));
+				_returnIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, new Intent(WwlLauncherDemoActivity.this, WwlLauncherDemoActivity.class));
+				sendBroadcast(_returnIntent);
+			}
+		});
 	}
 }
